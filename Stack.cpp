85bee1a0d2d2e822a01718d0e757cf4d3c104eb0
Stacy <nastya.vcg@gmail.com>
2017-03-01 01:23:01 +0300

    test commit

---
+++ Stack.cpp
@@ -0,0 +1,29 @@
+template <class T>
+class Stack {
+public:
+	Stack();
+	~Stack();
+	T push(T value);
+	void pop();
+	T top();
+	bool empty();
+	int size();
+
+private:
+	T* container;
+};
+
+template <class T>
+T Stack::push(int value) {
+	
+}
+
+template <class T>
+void Stack::pop() {
+	
+}
+
+template <class T>
+int Stack::top() {
+	
+}
